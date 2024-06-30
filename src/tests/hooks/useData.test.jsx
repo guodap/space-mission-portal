ddfa9791dc2ba40262c565c6429fe4f9b453e7d3
Guoda <33546350+guodap@users.noreply.github.com>
2024-06-30 20:51:12 +0200

    Add placeholder tests

--- src/tests/hooks/useData.test.jsx
+++ src/tests/hooks/useData.test.jsx
@@ -1,5 +1,5 @@
-describe("Empty test suite", () => {
-  it("should pass without any tests", () => {
+describe("Need to write component and hook tests. So far, only wrote util tests...", () => {
+  it("but should pass without any tests for now", () => {
     expect(true).toBe(true);
   });
 });
